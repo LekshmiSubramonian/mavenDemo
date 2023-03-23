fd8dd718612904f13c977596065513a187f86ff9
LekshmiSubramonian <lekshmi.mani74@gmail.com>
2023-03-24 02:04:03 +0530

    changes

--- src/main/java/com/sample/mavenDemo/App.java
+++ src/main/java/com/sample/mavenDemo/App.java
@@ -13,10 +13,26 @@ public class App
     	
     	ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
     	
-    	Vehicle obj = (Vehicle)context.getBean("car"); 
+//    	Vehicle obj = (Vehicle)context.getBean("vehicle"); 
+    	
+    	//using xml
     	// getBean will give the object of the object(obj) so typecast to vehicle
     	//applicationcontext will give the object of vehicle but it doesnt know what vehicle is that
     	// to do that create a xml file => mention which object u need whenever u call a vehicle
-    	obj.drive();
+    	
+    	// using annotation
+    	//add @ in java class
+    	//add component-scan in spring.xml
+    	
+//    	obj.drive();
+    	
+    	//using java configuration
+    	
+//    	Tyre t = new Tyre();
+//    	//new keyword creates a new dependency so do the following
+    	Tyre t = (Tyre)context.getBean("tyre");
+    	System.out.println(t);
+    	
+    	
     }
 }
